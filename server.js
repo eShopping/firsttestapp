a3f66353ed801439b86f81681d23ea9251949b72
princesharma78 <princesharma78@gmail.com>
2018-09-11 01:04:49 +0530

    index and update server file

--- server.js
+++ server.js
@@ -99,11 +99,11 @@ request
 
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angularShop'));
+app.use(express.static(__dirname + '/dist/firsttestapp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angularShop/index.html'));
+res.sendFile(path.join(__dirname+'/dist/firsttestapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
